2e502b9d4f640115925d0a65f91da15e7abae634
tusharmishra-25 <jai2mishra5@gmail.com>
2025-11-09 16:31:53 +0530

    Update RestaurantMenuServiceApplication.java

---
+++ src/main/java/com/fooddelivery/restaurantmenuservice/RestaurantMenuServiceApplication.java
@@ -0,0 +1,12 @@
+package com.fooddelivery.restaurantmenuservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestaurantMenuServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestaurantMenuServiceApplication.class, args);
+    }
+}
